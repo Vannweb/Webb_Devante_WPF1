ba9f8f973d8ca46a8919f0b465962154ca5a372a
Vannweb <dawebb@fullsail.edu>
2015-01-22 02:05:03 -0800

    Conditionals worksheet
    conditionals worksheet

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -1,1 +1,8 @@
-alert("This is in the js file");
+alert("We're going to figure out if you need to stop at the next gas station.");
+var gasEff = Number(prompt("Please enter the gas efficiency of the car(in mpg):"));
+var reading = Number(prompt("At what percent is your gas gauge?(From 0 to 100)"));
+var capacity = Number(prompt("How much gas, in gallons, can your car hold?"));
+
+var readingPerc = reading / 100;
+
+
