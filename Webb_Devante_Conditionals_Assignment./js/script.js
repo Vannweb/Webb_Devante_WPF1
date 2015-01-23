2e332469a5a4b711d0b680e18eaed93831286231
Vannweb <dawebb@fullsail.edu>
2015-01-22 19:16:11 -0800

    conditionals Assignment
    progress check in

--- Webb_Devante_Conditionals_Assignment./js/script.js
+++ Webb_Devante_Conditionals_Assignment./js/script.js
@@ -1,1 +1,9 @@
-alert("This is in the js file");
+alert("Okay intern, it's time to pick up coffee for the office.");
+var people = Number(prompt("First we need to know how many people are in your office:"));
+var budget = Number(prompt("How much are we looking to spend today?"));
+var preference = prompt("Next we need to know what kind of caffeine we're buying, lattes or coffee:");
+
+if (preference == "lattes" || preference == "Lattes" ){
+    var total = people * 
+}
+
