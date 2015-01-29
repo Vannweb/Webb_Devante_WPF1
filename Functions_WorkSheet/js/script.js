dde49eb454baf296158778600dfa876cd06e615c
Vannweb <dawebb@fullsail.edu>
2015-01-29 12:56:52 -0800

    Functions Worksheet
    Began the functions worksheet

---
+++ Functions_WorkSheet/js/script.js
@@ -0,0 +1,11 @@
+//Functions Worksheet Devante Webb 1/28/2015
+
+alert("Hello! I will help you find the circumference of a circle.");
+
+var radius = Number(prompt("What is the radius of the circle?");
+
+// var circumference =
+
+function calCirc(r){
+    return r * 2;
+}
