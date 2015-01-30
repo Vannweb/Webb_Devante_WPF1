a9b5174d4c765078c7961bbec9f21685a9cfe831
Vannweb <dawebb@fullsail.edu>
2015-01-29 18:11:45 -0800

    functions Worksheet
    Making the function

--- Functions_WorkSheet/js/script2.js
+++ Functions_WorkSheet/js/script2.js
@@ -9,3 +9,6 @@ alert("Welcome, bee stings can be excruciating and sometimes deadly. It could he
 var weight = Number(prompt("First we need to know how much you weigh. We'll do the rest from there. \nEnter your " +
 "weight in pounds:"));
 
+function stingCalc(lbs){
+    return lbs * 
+}
