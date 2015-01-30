62cf7af10f05e34e47e63fa0c6ebda1a093490ad
Vannweb <dawebb@fullsail.edu>
2015-01-29 18:34:06 -0800

    Functions Assignment
    Finished commenting, finished the assignments

--- Functions_WorkSheet/js/script2.js
+++ Functions_WorkSheet/js/script2.js
@@ -12,9 +12,12 @@ var weight = Number(prompt("First we need to know how much you weigh. We'll do t
 "weight in pounds:"));
 //This line defines the variable result and assigns the functions result to the variable result. Also invokes the function
 var result = stingCalc(weight);
-//
+//this line creates the function were we find out how many stings it takes to kill whomever person is that the weight was entered for
+//also makes the parameters
 function stingCalc(lbs){
-    return lbs * 8.666666667
+    return lbs * 8.666666667; // this uses multiplication to find out how many stings it would take to kill someone of this weight
 }
 alert("It would take " + result + " bee stings to kill someone of that weight. Please refresh to try a new weight." +
-" Thank you.");
+" Thank you."); //I tell the user how many stings it would take to kill someone of this weight.
+console.log("It would take " + result + " bee stings to kill someone of that weight. Please refresh to try a new weight." +
+" Thank you."); //Here I print it to the console as well, as per instructions 
