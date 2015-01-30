f0e6a7d30f8fa34df228a99ea3cadbc4fc1c5c52
Vannweb <dawebb@fullsail.edu>
2015-01-29 20:20:13 -0800

    Functions Assignment
    Adding the while loops for validation

--- Webb_Devante_Functions_Assignment/js/script.js
+++ Webb_Devante_Functions_Assignment/js/script.js
@@ -6,12 +6,18 @@ Functions Assignment
 alert("So you're moving to San Francisco! \nThat's a big move. It's a beautiful city to live in, however it can get expensive." +
 " Here, we're gonna give you an idea of your expenses, and tell you how much space you'll have, to maybe better prepare you for your move.");
 
-var occupants = Number(prompt("First how many room mates will you have? We'll use this to figure the price of rent, or get an idea of it:"));
+var occupants = Number(prompt("First how many people are living here? We'll use this to figure the price of rent, or get an idea of it:"));
+while (occupants === "" || isNaN(occupants) || occupants < 1){
+    occupants = Number(prompt("I'm sorry we couldn't use that one. Please enter a number that is 1 or greater:"));
+}
+
 averageRent = 2100;
 
 var roomL = Number(prompt("Next we'll get an idea of how much space you'll have. Simply ask the landlord for the apartment specs." +
 "\nLet's begin with the length, in feet please:"));
-
+while(roomL === "" || isNaN(roomL)){
+    while =
+}
 var roomW = Number(prompt("Next find out the width of the room, and enter it here:"));
 
 var roomH = Number(prompt("Finally, we'll take the height in here:"));
@@ -36,5 +42,5 @@ alert("And the amount of space you should expect is "+ space + " square feet. I 
 alert("Living in San Francisco is expensive. However it can be a place of much opportunity and discovery. I hope you enjoy your home!");
 
 /*I tested the code by entering the values 1000 500 and 700, and forgot to write down the total! However I will be testing more
-
+I used the values dad mom spaghetti and 0 to test the first while loop
  */
